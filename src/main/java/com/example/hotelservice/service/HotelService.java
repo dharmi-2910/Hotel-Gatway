a20797f6362666a8ef2fae724724176102d8dd1e
abc <abc2@gmail.com>
2024-02-20 17:51:36 +0530

    hotel cometed

--- src/main/java/com/example/hotelservice/service/HotelService.java
+++ src/main/java/com/example/hotelservice/service/HotelService.java
@@ -4,7 +4,6 @@ import com.example.hotelservice.entities.Hotel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 import java.util.Optional;
 
